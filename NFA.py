10029b57fb5d648f59fea64dc8b3eae66c8aa760
kartikslopo <67042120+kartikslopo@users.noreply.github.com>
2021-03-31 09:27:31 +0530

    NFA

---
+++ NFA.py
@@ -0,0 +1,20 @@
+from automata.fa.nfa import NFA
+
+dfa = DFA(
+    states={'q0', 'q1', 'q2'},
+    input_symbols={'0', '1'},
+    transitions={
+        'q0': {'0': 'q0', '1': 'q1'},
+        'q1': {'0': 'q1', '1': 'q2'},
+        'q2': {'0': 'q2', '1': 'q2'}
+    },
+    initial_state='q0',
+    final_states={'q2'}
+)
+input_str = input('string =')
+if dfa.accepts_input(input_str):
+    print('accepted')
+else:
+    print('rejected')
+
+dfa.read_input('011')
